fdd9f1707dbd46a225105ea5c4eaa5f26a5549ef
cassido-fx <okaforcassy@gmail.com>
2024-05-08 11:42:27 -0400

    app.js and Navbar.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React, { useEffect } from 'react';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from './Components/navbar/Navbar.js';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Navbar from './Components/navbar/Navbar';
 function App() {
 
-  return (
-    <div className="App">
-        <BrowserRouter>
-          <navbar/>
-              <Routes>
-              </Routes>
-            
-        </BrowserRouter>
-       
-    </div>
-  
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Landing_Page/>}></Route>
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
